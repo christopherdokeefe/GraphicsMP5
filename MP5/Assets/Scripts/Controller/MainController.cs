1747f1d52c5c5735345c0b6cebb0c2772d39e7d1
christopherdokeefe <cdok@uw.edu>
2019-11-23 23:28:37 -0800

    Set up Cylinder Mesh
    -Set up the cylinder mesh classes (NxMMesh copies minus a few things)
    -Created sliders to control the number of vertices in the rows and columns
    -Changed the MainController to initiate and update the CylinderMesh
    -Commented a psuedo-code solution to how the vertices are placed in the MyMeshCylinder class

--- MP5/Assets/Scripts/Controller/MainController.cs
+++ MP5/Assets/Scripts/Controller/MainController.cs
@@ -7,18 +7,30 @@ public partial class MainController : MonoBehaviour
     // UNKNOWN PROBLEM WITH GETTING MyMesh COMPONENT
     // INITIALIZING AND UPDATING MESH IN MyMesh FOR NOW
     public GameObject QuadNxM;
+    public GameObject Cylinder;
     private MyMeshNxM myMeshNxM;
+    private MyMeshCylinder myMeshCylinder;
 
-    public SliderWithEcho vertexPerRowSlider;
-    public SliderWithEcho vertexPerColumnSlider;
+    public SliderWithEcho quadVertexPerRowSlider;
+    public SliderWithEcho quadVertexPerColumnSlider;
+
+    public SliderWithEcho cylinderVertexPerRowSlider;
+    public SliderWithEcho cylinderVertexPerColumnSlider;
     // Start is called before the first frame update
     void Start()
     {
         myMeshNxM = QuadNxM.GetComponent<MyMeshNxM>();
-        myMeshNxM.InitializeMesh((int) vertexPerRowSlider.GetSliderValue(), (int) vertexPerColumnSlider.GetSliderValue());
+        myMeshNxM.InitializeMesh((int) quadVertexPerRowSlider.GetSliderValue(), (int) quadVertexPerColumnSlider.GetSliderValue());
 
-        vertexPerRowSlider.TheSlider.onValueChanged.AddListener(resizeMesh);
-        vertexPerColumnSlider.TheSlider.onValueChanged.AddListener(resizeMesh);
+        myMeshCylinder = Cylinder.GetComponent<MyMeshCylinder>();
+        myMeshCylinder.InitializeMesh((int)cylinderVertexPerRowSlider.GetSliderValue(), (int)cylinderVertexPerColumnSlider.GetSliderValue());
+
+
+        quadVertexPerRowSlider.TheSlider.onValueChanged.AddListener(resizeQuadMesh);
+        quadVertexPerColumnSlider.TheSlider.onValueChanged.AddListener(resizeQuadMesh);
+
+        cylinderVertexPerRowSlider.TheSlider.onValueChanged.AddListener(resizeCylinderMesh);
+        cylinderVertexPerColumnSlider.TheSlider.onValueChanged.AddListener(resizeCylinderMesh);
 
         vertexHighlightColor = new Color(0.5f, 0f, 0.3f, 0.5f);
         axisFrameHighlightColor = new Color(0.8f, 0.8f, 0.1f, 0.5f);
@@ -29,14 +41,23 @@ public partial class MainController : MonoBehaviour
     void Update()
     {
         myMeshNxM.UpdateMesh();
+        myMeshCylinder.UpdateMesh();
+
         checkObjectSelection();  // Select any vertexObject that has been clicked
         checkVertexDisplay();
     }
 
-    void resizeMesh(float v)
+    void resizeQuadMesh(float v)
     {
-        int vertexRows = (int) vertexPerRowSlider.GetSliderValue();  // Change value to an int for InitializeMesh to use
-        int vertexColumns = (int) vertexPerColumnSlider.GetSliderValue();
+        int vertexRows = (int) quadVertexPerRowSlider.GetSliderValue();  // Change value to an int for InitializeMesh to use
+        int vertexColumns = (int) quadVertexPerColumnSlider.GetSliderValue();
         myMeshNxM.InitializeMesh(vertexRows, vertexColumns);
+    }
+
+    void resizeCylinderMesh(float v)
+    {
+        int vertexRows = (int)cylinderVertexPerRowSlider.GetSliderValue();  // Change value to an int for InitializeMesh to use
+        int vertexColumns = (int)cylinderVertexPerColumnSlider.GetSliderValue();
+        myMeshCylinder.InitializeMesh(vertexRows, vertexColumns);
     }
 }
--- MP5/Assets/Scripts/World/MyMeshCylinder/MyMeshCylinder.cs
+++ MP5/Assets/Scripts/World/MyMeshCylinder/MyMeshCylinder.cs
@@ -2,29 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public partial class MyMeshNxM : MonoBehaviour 
+public partial class MyMeshCylinder : MonoBehaviour
 {
-    // For passing value into ComputeNormals in UpdateMesh()
     int rows;
     int columns;
-    // n by m number of vertices - 3x2 mesh = 4x3 vertices
-    // Vn: number of vertices in a row
-    // Vm: number of vertices in a column
-	public void InitializeMesh(int Vn, int Vm) 
+
+    // Vn: number of verticies on y-axis
+    // Vm: number of verticies around circumfrance
+    public void InitializeMesh(int Vn, int Vm)
     {
         Mesh theMesh = GetComponent<MeshFilter>().mesh;   // get the mesh component
         theMesh.Clear();    // delete whatever is there!!
 
-        rows = Vn;  
+        rows = Vn;
         columns = Vm;
 
         Vector3[] v = new Vector3[Vn * Vm];   // n by n mesh needs (n + 1) by (n + 1) vertices
         Vector3[] n = new Vector3[Vn * Vm];   // Vertex normals - Must be the same size as v
-        Vector2[] uv = new Vector2[Vn * Vm];  // uv controls texture transform on mesh
 
         // Number of triangles: (n - 1) by (n - 1) mesh and 
         // 2 triangles on each mesh-unit and 3 indexes per triangle
-        int[] t = new int[(Vn - 1) * (Vm - 1) * 2 * 3];         
+        int[] t = new int[(Vn - 1) * (Vm - 1) * 2 * 3];
 
         // Initialize the vertices with x and z from -1 to 1                                        v[12] v[13] v[14] v[15]
         // Note: Calculates from 0 to 2 for convenience, then subtracts 1 in inner for loop         v[8]  v[9]  v[10] v[11]
@@ -37,10 +35,11 @@ public partial class MyMeshNxM : MonoBehaviour
         {
             for (int j = 0; j < Vn; j++)
             {
-                // (i * Vn) moves the vertex index to the next row
-                //  j moves the vertex's column position, i.e. it moves it along the row
-                // Example: if Vn = 4 (4 vertices per row), i = 0 loop sets v[0], v[1], v[2], v[3],  i = 1 sets v[4], v[5], v[6], v[7], and so on
-                // (j * interval) - 1 spaces vertices evenly from -1 to 1 on the x axis, and same for (i * interval) - 1
+                // Following line to change to something like: 
+                //
+                // (r * cos(theta), j * height, (r * sin(theta))
+                // radius = r
+                // theta = the value of is a angle between 10 and 360 degrees divided by the Vm (rows)
                 v[(i * Vn) + j] = new Vector3((j * intervalN) - 1, 1, (i * intervalM) - 1);
             }
         }
@@ -52,7 +51,7 @@ public partial class MyMeshNxM : MonoBehaviour
         }
 
         // if Vn = 4, this = 18 since there are 3 mesh units per row * 2 triangles per mesh unit * 3 indices per triangle)
-        int triangleIndicesPerRow = (Vn - 1) * 2 * 3; 
+        int triangleIndicesPerRow = (Vn - 1) * 2 * 3;
 
         // Initialize the triangles using the vertices' indexes
         // For loop explanation: For this explanation, assume Vn = 4 (4 vertices per row)
@@ -72,10 +71,10 @@ public partial class MyMeshNxM : MonoBehaviour
             for (int j = 0; j < Vn - 1; j++)  // Goes through each mesh unit (2 triangles) in a row
             {
                 // Creates first triangle of the mesh unit
-                t[i + (j * 6)]     = (0) + j + ((i / triangleIndicesPerRow) * Vn);
+                t[i + (j * 6)] = (0) + j + ((i / triangleIndicesPerRow) * Vn);
                 t[i + (j * 6) + 1] = (Vn) + j + ((i / triangleIndicesPerRow) * Vn);
                 t[i + (j * 6) + 2] = (Vn + 1) + j + ((i / triangleIndicesPerRow) * Vn);
-                
+
                 // Creates second triangle of the mesh unit
                 t[i + (j * 6) + 3] = (0) + j + ((i / triangleIndicesPerRow) * Vn);
                 t[i + (j * 6) + 4] = (Vn + 1) + j + ((i / triangleIndicesPerRow) * Vn);
@@ -83,23 +82,9 @@ public partial class MyMeshNxM : MonoBehaviour
             }
         }
 
-        // handles uv 
-        float uvIntervalN = 1f / (Vn - 1);
-        float uvIntervalM = 1f / (Vm - 1);
-
-        // Translate all the vertices to UV values between 0 and 1 (U, V)
-        for (int i = 0; i < Vm; i++)
-        {
-            for (int j = 0; j < Vn; j++)
-            {
-                uv[(i * Vn) + j] = new Vector2(j * uvIntervalN, i * uvIntervalM);
-            }
-        }
-
         theMesh.vertices = v; //  new Vector3[3];
         theMesh.triangles = t; //  new int[3];
         theMesh.normals = n;
-        theMesh.uv = uv;
 
         // Uses MyMesh_Controllers partial class to create all the spheres to control the vertices and their normals
         createVertexObjects(v);
@@ -112,18 +97,21 @@ public partial class MyMeshNxM : MonoBehaviour
         Mesh theMesh = GetComponent<MeshFilter>().mesh;
         Vector3[] v = theMesh.vertices;
         Vector3[] n = theMesh.normals;
-        int [] t = theMesh.triangles;
+        int[] t = theMesh.triangles;
 
-        for (int i = 0; i < vertexObjects.Length; i++)
+        if (vertexObjects != null)
         {
-            v[i] = vertexObjects[i].transform.localPosition;
+            for (int i = 0; i < vertexObjects.Length; i++)
+            {
+                v[i] = vertexObjects[i].transform.localPosition;
+            }
+
+            ComputeNormals(v, n, t, rows, columns);
+
+            theMesh.vertices = v;
+            theMesh.normals = n;
         }
-
-        ComputeNormals(v, n, t, rows, columns);
-
-        theMesh.vertices = v;
-        theMesh.normals = n;
-	}
+    }
 
     // Clear the mesh and delete all vertex objects
     public void DeleteMesh()
@@ -140,3 +128,5 @@ public partial class MyMeshNxM : MonoBehaviour
         }
     }
 }
+
+
